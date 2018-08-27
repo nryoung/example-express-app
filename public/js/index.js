5c680cd00e0c5c951191f65b2c94fccd42ce4547
Nic Young <nryoung@gmail.com>
2018-08-26 22:20:52 -0600

    clean up the code a little more

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,3 @@
-const handleSuccess = () => {
-  console.log('success!');
-  let words = this.responseText;
-  console.log(words);
-  let wordsArray = words.split('\n');
-
-  console.log(wordsArray);
-};
-
-const handleError = () => {
-  console.log('fail!');
-};
-
 const request = new XMLHttpRequest();
 request.onload = () => {
   const words = request.responseText;
